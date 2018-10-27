cb36aa3918b548ba01188bff5abbd14ad9ee66e6
Aleksey Ilyin <alksily@icloud.com>
2018-10-27 10:16:47 +0300

    fix http check exist files array

--- src/Http/UploadedFile.php
+++ src/Http/UploadedFile.php
@@ -84,7 +84,7 @@ class UploadedFile implements UploadedFileInterface
     {
         $env = collect($globals);
 
-        if (is_array($env['orchid.files']) && $env->has('orchid.files')) {
+        if ($env->has('orchid.files') && is_array($env['orchid.files'])) {
             return $env['orchid.files'];
         } elseif (isset($_FILES)) {
             return static::parseUploadedFiles($_FILES);
